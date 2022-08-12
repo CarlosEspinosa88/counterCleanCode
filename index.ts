778b91ee3c62f359bef40e503f08b9a46d050ad1
Carlos Espinosa <carloespinosa88@gmail.com>
2022-08-11 21:16:25 -0500

    video(1): add basic types

--- index.ts
+++ index.ts
@@ -1,5 +1,37 @@
 let userName: string = 'Carlos Espinosa';
 let hasLoggin: boolean = true;
 
-console.log(typeof hasLoggin);
-console.log(typeof userName);
+const names: string[] = userName.split(' ')
+const numbers: number[] = [10, 20]
+const arrayNumbers: Array<number> = [1.2, 2.5]
+
+interface Person {
+  first: string
+  last: string
+}
+
+const myName: Person = {
+  first: 'Carlos',
+  last: 'Espinosa'
+}
+
+const ids: Record<number, string> = {
+  10: "a",
+  20: "b"
+}
+
+ids[30] = "c"
+
+if (ids[30] === "c") {
+  console.log("entro")
+}
+
+const mapNumer: number[] = [1,2,3].map((item) => item * 10)
+
+console.log(names)
+console.log(numbers)
+console.log(arrayNumbers)
+console.log(myName)
+console.log(ids)
+console.log(mapNumer)
+
